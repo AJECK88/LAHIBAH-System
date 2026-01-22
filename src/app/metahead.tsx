c03a1a519a4a53d74e942e616e8dc8c4c18f284d
AJECK88 <eajeckelizee@mail.com>
2026-01-22 04:59:46 +0100

    updating the meta Head

--- src/app/metahead.tsx
+++ src/app/metahead.tsx
@@ -1,11 +1,19 @@
 import Head from "next/head";
-export default function MetaHead(
-) {
+
+export default function MetaHead() {
   return (
     <Head>
-    <meta property="og:image" content="/Lahiba.png" />
-    <meta property="og:image:width" content="1200" />
-    <meta property="og:image:height" content="630" />
-   </Head>
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="LAHIBA SCHOOL MANAGEMENT SYSTEM" />
+      <meta
+        property="og:image"
+        content="http://lahibah-system.vercel.app/image/Lahiba.png"
+      />  <meta
+        property="og:description"
+        content="Lahibah System is an all-in-one platform designed to streamline and enhance the management of educational institutions. It offers a comprehensive suite of tools and features that cater to the needs of administrators, teachers, students, and parents alike."
+      />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
+    </Head>
   );
 }
