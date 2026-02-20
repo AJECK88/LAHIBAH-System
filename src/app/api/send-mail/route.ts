8a1109b161307d784ffb14bf4d8bb940998967e7
AJECK88 <eajeckelizee@mail.com>
2026-02-20 02:18:14 +0100

    updating the mail sender api

--- src/app/api/send-mail/route.ts
+++ src/app/api/send-mail/route.ts
@@ -1,9 +1,14 @@
-import { NextResponse } from "next/server";
 import nodemailer from "nodemailer";
 
-export async function POST(req: Request) {
-  const { to, subject, html } = await req.json();
-
+export async function sendMail({
+  to,
+  subject,
+  html,
+}: {
+  to: string;
+  subject: string;
+  html: string;
+}) {
   const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
@@ -12,20 +17,10 @@ export async function POST(req: Request) {
     },
   });
 
-  try {
-    await transporter.sendMail({
-      from: process.env.EMAIL_USER,
-      to,
-      subject,
-      html
-    });
-
-    return NextResponse.json({ success: true });
-  } catch (error) {
-    console.error("Error sending email:", error);
-    return NextResponse.json(
-      { success: false, error },
-      { status: 500 }
-    );
-  }
+  return transporter.sendMail({
+    from: process.env.EMAIL_USER,
+    to,
+    subject,
+    html,
+  });
 }
