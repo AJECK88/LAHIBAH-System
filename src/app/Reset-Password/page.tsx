398873bae8c6638d7ce20546412a6879c3f65543
AJECK88 <eajeckelizee@mail.com>
2026-06-23 15:52:17 +0100

    working an the verification and validation for users password reset

--- src/app/Reset-Password/page.tsx
+++ src/app/Reset-Password/page.tsx
@@ -2,6 +2,7 @@
 
 import verificationEmail from "./actions"
 import { useRouter } from "next/navigation"
+import Image from "next/image"
 import Link  from "next/link"
 const  ResetPassword =  (
 )=>{
@@ -41,14 +42,14 @@ const  ResetPassword =  (
      return (
         <div className="w-full h-screen flex items-center justify-center">
          <form onSubmit={handleSubmit}
-          className=" space-y-6 rounded-2xl bg-white px-4 py-10 shadow-md ring-1 ring-black/5 sm:w-96 sm:px-8 flex-col"
+          className=" space-y-5 rounded-2xl bg-white px-4 py-10 shadow-md ring-1 ring-black/5 sm:w-96 sm:px-8 flex-col"
          >
          <h1 className="font-semibold text-gray-700">Password Reset</h1>
           <label htmlFor="Email">Email <strong className="text-red-700">*</strong></label>
-          <input className="w-full p-2 border" type="email" placeholder="example@gmail.com" required name="Email"/>
+          <input className="w-full p-2 border rounded-sm" type="email" placeholder="example@gmail.com" required name="Email"/>
           <h3>{}</h3>
-         <button className="w-full bg-blue-500 p-2 text-white consure  font-semibold">Reset Password </button>
-         <Link href = '/sign-in' className="text-blue-600 flex justify-end w-full">Back to Login</Link> 
+         <button className="w-full bg-blue-500 p-2 text-white consure  font-semibold rounded-sm">Reset Password </button>
+         <Link href = '/sign-in' className="text-blue-600 flex justify-end w-full underline ">Back to Login <Image alt="arrow" src={"/arrow.png"} width={20} height={20} /> </Link> 
          </form>
      
         </div>
